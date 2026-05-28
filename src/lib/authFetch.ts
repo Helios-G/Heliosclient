545ce9ae2731cff6f91a55d0bf742d606ada694c
kallin <kedia1004@gmail.com>
2026-05-28 18:28:56 +0900

    경로수정

--- src/lib/authFetch.ts
+++ src/lib/authFetch.ts
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://localhost:8081";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8081";
 
 export const authFetch = async (url: string, options: RequestInit = {}) => {
 
